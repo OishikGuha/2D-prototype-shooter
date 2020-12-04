afdcc66b446e517937c68dff1e16b66829892c65
Roboticol <kaushikguha68@gmail.com>
2020-12-05 00:39:57 +0530

    Add the main project

---
+++ Assets/OriginMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginMovement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float zAxis = transform.position.z;
+
+        //Mouse Input
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 mouseLook = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0f));
+
+        transform.position = mouseLook;
+        Mathf.Clamp(zAxis, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public float xMouse;
+    public float yMouse;
+
+    Vector2 movement;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            moveSpeed += 10;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            moveSpeed -= 10;
+        }
+
+        //Look at mouse
+        FaceMouse();
+
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void FaceMouse() 
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        xMouse = direction.x;
+        yMouse = direction.y;
+        transform.up = direction;
+    }
+}
