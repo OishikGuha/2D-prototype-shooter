f5602e57e8dc21855c5ef4c159adc0432742cdc1
Roboticol <kaushikguha68@gmail.com>
2020-12-06 18:48:31 +0530

    Add crosshair, screen shake, bullet mechanics, etc.

---
+++ Assets/CrossHairScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHairScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        rb.position = mousePos;
+        
+    }
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,8 +7,16 @@ public class Bullet : MonoBehaviour
 
     public GameObject hiteffect;
 
+    private Shake shake;
+
+    private void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<Shake>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        shake.CamShake();
         GameObject effect = Instantiate(hiteffect, transform.position, Quaternion.identity);
         Destroy(effect, 1f);
         Destroy(gameObject);
