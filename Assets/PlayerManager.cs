49ec1fc9305a46277a52ee14be72444cba1dc178
Roboticol <kaushikguha68@gmail.com>
2020-12-12 14:47:49 +0530

    Added som game mechanics

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public Animator animate;
+
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void PDamage(int damage, bool animation) 
+    {
+        health -= damage;
+        animate.SetBool("Damaging", animation);
+
+        if (health <= 0) 
+        {
+            Die();
+        }
+    }
+
+    void Die() 
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemy") 
+        {
+            PDamage(10, true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemy") 
+        {
+            PDamage(10, false);
+        }
+    }
+
+}
